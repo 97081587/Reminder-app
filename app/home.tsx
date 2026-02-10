319d9da222718ecb77fa89418b00e7b4e050a591
Oscar <97081587@st.deltion.nl>
2026-02-10 15:44:16 +0100

    routes

--- app/home.tsx
+++ app/home.tsx
@@ -1,5 +1,33 @@
-import { Text, View } from "react-native";
+import {StyleSheet, Text, View } from "react-native";
 
 export default function Home() {
-  return <Home />;
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text style={styles.text}>Reminders</Text>
+      <View style={styles.div}></View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#DFA355",
+    alignItems: "center",
+  },
+  text: {
+    color: "#ffffff",
+    marginTop: 60,
+    fontSize: 35,
+  },
+  div: {
+    width: 70,
+    height: 70,
+    backgroundColor: "#ffffff",
+    borderRadius: 100,
+    // blurRadius: 1000,
+
+  },
+});
