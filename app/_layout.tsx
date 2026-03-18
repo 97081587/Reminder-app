2568a9a4332cee416614f7143713bfddb3c58c01
osaf1 <zsafi073@gmail.com>
2026-03-18 14:41:44 +0100

    date and time is fixed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,10 @@
 import { Stack } from "expo-router";
 
-<<<<<<< HEAD
 // export default function RootLayout() {
 //   return <Stack />;
 // }
 
-=======
+
 export default function RootLayout() {
   return <Stack />;
 }
->>>>>>> b3e1051a83dbca30ffe2c419791b76baa5dd98ad
