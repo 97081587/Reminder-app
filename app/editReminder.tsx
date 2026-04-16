2b069026867a67e89600a8065b2e41cc0b45c13b
Oscar <97081587@st.deltion.nl>
2026-04-16 11:48:47 +0200

    style: update text color for Edit Reminder button

--- app/editReminder.tsx
+++ app/editReminder.tsx
@@ -131,7 +131,7 @@ export default function EditReminder() {
               style={styles.addBtn}
               onPress={saveEditedReminder}
             >
-              <Text>Edit Reminder</Text>
+              <Text style={{ color: "white" }}>Edit Reminder</Text>
             </TouchableOpacity>
           </View>
         </View>
