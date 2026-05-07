8e173a6ea6f63f6dcae1b5adc34daa7ac41c7b89
Oscar <97081587@st.deltion.nl>
2026-05-07 09:08:22 +0200

    fix: update lastUsed timestamp in devices.json and refactor ReminderCard component in index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -37,14 +37,7 @@ export default function Home() {
   //reminder cards
   const ReminderCard = ({ item, onDelete }) => {
     return (
-      <View
-        style={{
-          width: "200%",
-          backgroundColor: "rgba(255,255,255,0.3)",
-          borderRadius: 30,
-          padding: 25,
-        }}
-      >
+      <View style={styles.reminderCard}>
         <Text>{item.title}</Text>
         <TouchableOpacity onPress={() => onDelete(item.id)}>
           <Text>🗑️</Text>
@@ -75,7 +68,10 @@ export default function Home() {
         {/* add button */}
         <TouchableOpacity style={styles.addWrap}>
           <Link href="/newReminder">
-            <View style={styles.div} />
+            <View style={styles.div}>
+              <Text style={styles.addText}>+</Text>
+            </View>
+            {/* <View style={styles.div} /> */}
           </Link>
         </TouchableOpacity>
       </View>
@@ -93,15 +89,28 @@ const styles = StyleSheet.create({
     marginTop: 60,
     fontSize: 35,
   },
-  div: {
-    width: 70,
-    height: 70,
-    backgroundColor: "#fff",
-    borderRadius: 100,
-  },
   addWrap: {
     position: "absolute",
     bottom: 40,
     right: 25,
+  },
+    div: {
+    width: 70,
+    height: 70,
+    backgroundColor: "rgba(255,255,255,0.3)",
+    borderRadius: 100,
+  },
+  addText: {
+    fontSize: 50,
+    color: "#2a8c82",
+    position: "absolute",
+    top: 1,
+    left: 22,
+  },
+  reminderCard: {
+    width: "200%",
+    backgroundColor: "rgba(255,255,255,0.3)",
+    borderRadius: 30,
+    padding: 25,
   },
 });
