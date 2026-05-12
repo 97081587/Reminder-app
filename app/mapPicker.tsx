c7e3caf442be3b10452c39e7e9dff31bc4f124a5
Oscar <97081587@st.deltion.nl>
2026-05-12 11:22:00 +0200

    fix: remove unused Location import in MapPicker component

--- app/mapPicker.tsx
+++ app/mapPicker.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { View, Button, StyleSheet } from "react-native";
 import MapView, { Marker } from "react-native-maps";
-import * as Location from "expo-location";
+// import * as Location from "expo-location";
 import { useRouter } from "expo-router";
 
 export default function MapPicker() {
